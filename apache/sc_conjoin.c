cffe64e651dd332ad0a03fc83b8fe48086efcc15
zhiwen.mizw <zhiwen.mizw@alibaba-inc.com>
2013-11-03 20:22:36 +0800

    md5 bugfix

--- apache/sc_conjoin.c
+++ apache/sc_conjoin.c
@@ -10,6 +10,7 @@
 #include "sc_conjoin.h"
 #include "sc_string.h"
 #include "apr_strings.h"
+#include "apr_md5.h"
 
 short sc_pool_create(sc_pool_t **newpool, sc_pool_t *parent) {
 	return apr_pool_create_ex(newpool, parent, NULL, NULL);
@@ -42,5 +43,5 @@ short sc_thread_mutex_unlock(sc_thread_mutex_t *mutex) {
 
 short sc_md5(unsigned char digest[SC_MD5_DIGESTSIZE], const void *input,
 		size_t inputLen) {
-	return sc_md5(digest, input, inputLen);
+	return apr_md5(digest, input, inputLen);
 }
